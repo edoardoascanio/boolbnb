4af5277f38871b4c6ab9de35c57c54a264202bc3
alessiotoni <toniellialessio@gmail.com>
2021-08-01 15:26:36 +0200

    add style

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,27 +1,164 @@
-@extends('layouts.mapLayout')
+@extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="home-container">
+
+    <section class="hero">
+        <div class="container">
+            <div class="row">
+                <div class="card col-lg-4 col-md-6 col-sm-12 col-xs-12">
+                    <div class="card-body">
+                        <h1>Affitti per le tue vacanze</h1>
+                        <p>Prenota un alloggio su Boolbnb</p>
+
+                        <form action="{{ route('guest.map') }}" method="post">
+                            @csrf
+                            <input type="text" id="city" name="city" placeholder="Città">
+                            <input type="text" id="number_beds" name="number_beds" placeholder="Numero Letti">
+                            <button class="btn btn-primary search-btn blu-btn"> <i class="fa fa-search"></i> <strong>Cerca</strong></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="city">
+        <div class="container">
+            <h3>Le città del momento</h3>
+
+            <div class="city-img row">
+
+                <div class="city-card col-md-3 col-sm-6">
+                    <a href=""><img src="{{asset('imgs/Roma.jpg')}}" alt="homepage"></a>
+                    <div class="caption">
+                        <strong>Roma</strong>
+                    </div>
+                </div>
+
+                <div class="city-card col-md-3 col-sm-6">
+                    <a href=""><img src="{{asset('imgs/Milano.jpg')}}" alt="homepage"></a>
+                    <div class="caption">
+                        <strong>Milano</strong>
+                    </div>
+                </div>
+
+                <div class="city-card col-md-3 col-sm-6">
+                    <a href=""><img src="{{asset('imgs/venezia.jpg')}}" alt="homepage"></a>
+                    <div class="caption">
+                        <strong>Venezia</strong>
+                    </div>
+                </div>
+
+                <div class="city-card col-md-3 col-sm-6">
+                    <a href=""><img src="{{asset('imgs/firenze.jpg')}}" alt="homepage"></a>
+                    <div class="caption">
+                        <strong>Firenze</strong>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="house-type">
+        <div class="container">
+            <h3>Una casa per ogni tuo bisogno</h3>
+            <p>Scegli case o stanze che più si addicono ai tuoi gusti e bisogni personali</p>
+
+            <div class="type-content row">
+                <div class="type-card col-md-4">
+                    <div class="type-img">
+                        <img src="{{asset('imgs/casa-natura.jpg')}}" alt="homepage">
+                        <div class="overlay"><i class="fa fa-home" aria-hidden="true"></i></div>
+                    </div>
+                    <div class="caption">
+                        <strong>Soggiorni nella natura</strong>
+                    </div>
+                </div>
+
+                <div class="type-card col-md-4">
+                    <div class="type-img">
+                        <img src="{{asset('imgs/casa-intera.jpg')}}" alt="homepage">
+                        <div class="overlay"><i class="fa fa-home" aria-hidden="true"></i></div>
+                    </div>
+                    <div class="caption">
+                        <strong>Case intere</strong>
+                    </div>
+                </div>
+
+                <div class="type-card col-md-4">
+                    <div class="type-img">
+                        <img src="{{asset('imgs/hotel-room.jpg')}}" alt="homepage">
+                        <div class="overlay"><i class="fa fa-home" aria-hidden="true"></i></div>
+                    </div>
+                    <div class="caption">
+                        <strong>Hotel rooms</strong>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
 
 
+    <section class="servicies">
+        <div class="container">
+            <h3>Servizi che potresti trovare </h3>
 
+            <div class="row text-center">
 
+                <div class="servicies-card col-lg-2 col-md-12 col-sm-12 col-xs-12">
+                    <i class="fa fa-cutlery" aria-hidden="true"></i>
+                    <p><strong>Cucina</strong></p>
+                </div>
 
+                <div class="servicies-card col-lg-2 col-md-12 col-sm-12 col-xs-12">
+                    <i class="fa fa-wifi" aria-hidden="true"></i>
+                    <p><strong>Wi-fi</strong></p>
+                </div>
 
+                <div class="servicies-card col-lg-2 col-md-12 col-sm-12 col-xs-12">
+                    <i class="fa fa-bath" aria-hidden="true"></i>
+                    <p><strong>Piscina</strong></p>
+                </div>
 
+                <div class="servicies-card col-lg-2 col-md-12 col-sm-12 col-xs-12">
+                    <i class="fa fa-product-hunt" aria-hidden="true"></i>
+                    <p><strong>Parcheggio gratuito</strong></p>
+                </div>
 
-    <form action="{{ route('guest.map') }}" method="post">
-        @csrf
-        <label for="city">Città</label>
-        <input type="text" id="city" name="city">
-        <label for="number_beds">Numero Letti</label>
-        <input type="text" id="number_beds" name="number_beds">
-        <button>cerca</button>
+                <div class="servicies-card col-lg-2 col-md-12 col-sm-12 col-xs-12">
+                    <i class="fa fa-snowflake-o" aria-hidden="true"></i>
+                    <p><strong>Aria condizionata</strong></p>
+                </div>
+            </div>
+        </div>
+    </section>
 
-    </form>
+    <section class="register hidden-md">
 
+        <div class="reg-img">
+            <div class="container">
+                <h1>Registrati</h1>
+                <p><strong>Registrati su Boolbnb, per guadagnare <br> nuove opportunità e fare <br> piacevoli esperienze</strong></p>
 
+                <a href="{{ route('register') }}"><button class="btn btn-primary reg-btn">Registrati</button></a>
+            </div>
+        </div>
 
-
+    </section>
 </div>
+
 @endsection
+<script>
+    function search() {
+
+        $city = document.getElementById('city').value
+        $number_beds = document.getElementById('number_beds')
+        $link = document.getElementById('link')
+
+        document.getElementById('link').innerHTML = "<a href='http://127.0.0.1:8000/guest/index?city=" + city + "'>Cerca</a>"
+    }
+
+</script>
--- resources/views/logged/accomodation/edit.blade.php
+++ resources/views/logged/accomodation/edit.blade.php
@@ -2,18 +2,26 @@
 
 @section('content')
 @include('layouts.components.error')
-<div class="container">
+<div class="container create-container">
 
-    <p>* tutti i campi sono obbligatori</p>
+    <p> <i class="fa fa-asterisk"></i><span><em> <strong>Tutti i campi sono obbligatori</strong></em></p>
+
 
     <form action="{{ route("logged.update", $accomodation->id ) }}" method="post" enctype="multipart/form-data">
 
         @csrf
         @method('PUT')
+        <div class="row">
 
-        <div class="form-group">
-            <label for="title">Titolo</label>
-            <input type="text" id="title" name="title" class="form-control" value="{{ $accomodation->title }}">
+            <div class="form-group title col-6">
+                <label for="title">Titolo</label>
+                <input type="text" id="title" name="title" class="form-control" value="{{ $accomodation->title }}">
+            </div>
+
+            <div class="form-group load-img col-6">
+                <label for="placeholder">Carica img di copertina</label>
+                <input type="file" name="placeholder" id="placeholder" accept=".jpg, .png, .svg, .jpeg">
+            </div>
         </div>
 
         <div class="form-group">
@@ -21,164 +29,123 @@
             <textarea name="description" id="description" cols="30" rows="10" class="form-control">{{ $accomodation->description }}"</textarea>
         </div>
 
-        <div class="form-group">
-            <label for="number_rooms">Numero di Stanze</label>
-            <input type="number" name="number_rooms" id="number_rooms" cols="30" rows="10" class="form-control" placeholder="max: 20" value="{{ $accomodation->number_rooms }}">
+        <div class="row justify-content-between">
+            <div class="form-group col-lg-2 col-md-6">
+                <label for="number_rooms">Numero di Stanze</label>
+                <input type="number" name="number_rooms" id="number_rooms" cols="30" rows="10" class="form-control" placeholder="max: 20" value="{{ $accomodation->number_rooms }}">
+            </div>
+
+            <div class="form-group col-lg-2 col-md-6">
+                <label for="number_bathrooms">Numero di Bagni</label>
+                <input type="number" name="number_bathrooms" id="number_bathrooms" cols="30" rows="10" class="form-control" placeholder="max: 20" value="{{ $accomodation->number_bathrooms }}">
+            </div>
+
+            <div class="form-group col-lg-2 col-md-6">
+                <label for="number_beds">Numero di Letti</label>
+                <input type="number" name="number_beds" id="number_beds" cols="30" rows="10" class="form-control" placeholder="max: 20" value="{{ $accomodation->number_beds }}">
+            </div>
+
+            <div class="form-group col-lg-2 col-md-6">
+                <label for="square_mts">Metri quadrati</label>
+                <input type="number" name="square_mts" id="square_mts" cols="30" rows="10" class="form-control" value="{{ $accomodation->square_mts }}">
+            </div>
+
+            <div class="form-group col-lg-2 col-md-6">
+                <label for="price_per_night">Prezzo per Notte</label>
+                <input type="number" name="price_per_night" id="price_per_night" cols="30" rows="10" class="form-control" value="{{ $accomodation->price_per_night }}">
+            </div>
         </div>
 
-        <div class="form-group">
-            <label for="number_bathrooms">Numero di Bagni</label>
-            <input type="number" name="number_bathrooms" id="number_bathrooms" cols="30" rows="10" class="form-control" placeholder="max: 20" value="{{ $accomodation->number_bathrooms }}">
-        </div>
+        <div class="row check">
+            <div class="form-group col-lg-3 col-md-6">
+                <label for="check_in">check in</label>
+                <select name="check_in" id="check_in" class="form-control">
+                    <option value="" seletced> Scegli orario </option>
+                    @for($i = 0; $i < 24; $i++) <option value="{{ $i }}" {{ $i == $accomodation->check_in ? 'selected' : ""}}> {{ $i }}:00 </option>
+                        @endfor
+                </select>
+            </div>
 
-        <div class="form-group">
-            <label for="number_beds">Numero di Letti</label>
-            <input type="number" name="number_beds" id="number_beds" cols="30" rows="10" class="form-control" placeholder="max: 20" value="{{ $accomodation->number_beds }}">
-        </div>
-
-        <div class="form-group">
-            <label for="square_mts">Metri quadrati</label>
-            <input type="number" name="square_mts" id="square_mts" cols="30" rows="10" class="form-control" value="{{ $accomodation->square_mts }}">
-        </div>
-
-        <div class="form-group">
-            <label for="price_per_night">Prezzo per Notte</label>
-            <input type="number" name="price_per_night" id="price_per_night" cols="30" rows="10" class="form-control" value="{{ $accomodation->price_per_night }}">
-        </div>
-
-        <div class="form-group">
-            <label for="check_in">check in</label>
-            <select name="check_in" id="check_in">
-                <option value="" seletced> Scegli orario </option>
-                @for($i = 0; $i < 24; $i++) <option value="{{ $i }}" {{ $i == $accomodation->check_in ? 'selected' : ""}}> {{ $i }}:00 </option>
-                    @endfor
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="check_out">check out</label>
-            <select name="check_out" id="check_out">
-                <option value="" seletced> Scegli orario </option>
-                @for($i = 0; $i < 24; $i++) <option value="{{ $i }}" {{ $i == $accomodation->check_out ? 'selected' : ""}}> {{ $i }}:00 </option>
-                    @endfor
-            </select>
+            <div class="form-group col-lg-3 col-md-6">
+                <label for="check_out">check out</label>
+                <select name="check_out" id="check_out" class="form-control">
+                    <option value="" seletced> Scegli orario </option>
+                    @for($i = 0; $i < 24; $i++) <option value="{{ $i }}" {{ $i == $accomodation->check_out ? 'selected' : ""}}> {{ $i }}:00 </option>
+                        @endfor
+                </select>
+            </div>
         </div>
 
         {{-- AGGIUNGERE PLACEHOLDER IMG --}}
-        <div class="form-group">
-        <label for="placeholder">Carica img di copertina</label>
-            <input type="file" name="placeholder" id="placeholder" accept=".jpg, .png, .svg, .jpeg">
-        </div>
+
 
         {{-- services  --}}
         <div class="form-group">
-            @foreach($services as $service)
-            <label for="{{ $service->title }}">
-                <input type="checkbox" name="services[]" value="{{ $service->id }}" id="{{ $service->title }}">
-                {{ $service->title }}
-            </label>
-            @endforeach
+            <strong>Aggiungi servizi</strong>
+            <div class="row services">
+                @foreach($services as $service)
+                <div class="col-lg-4">
+                    <label for="{{ $service->title }}">
+                        <input type="checkbox" name="services[]" value="{{ $service->id }}" id="{{ $service->title }}">
+                        {{ $service->title }}
+                    </label>
+                </div>
+                @endforeach
+            </div>
         </div>
 
-        <div class="form-group">
-      <label for="country">Paese</label>
-      <input
-        type="text"
-        name="country"
-        id="country"
-        cols="30"
-        rows="10"
-        class="form-control"
-        value="{{ old('country', $accomodation->country) }}"
-      />
-    </div>
+        <div class="row">
+            <div class="form-group col-lg-3 col-md-6">
+                <label for="country">Paese</label>
+                <input type="text" name="country" id="country" cols="30" rows="10" class="form-control" value="{{ old('country', $accomodation->country) }}" />
+            </div>
 
-    <div class="form-group">
-      <label for="city">Citta'</label>
-      <input
-        type="text"
-        name="city"
-        id="city"
-        cols="30"
-        rows="10"
-        class="form-control"
-        value="{{ old('city', $accomodation->city) }}"
-      />
-    </div>
+            <div class="form-group col-lg-3 col-md-6">
+                <label for="city">Citta'</label>
+                <input type="text" name="city" id="city" cols="30" rows="10" class="form-control" value="{{ old('city', $accomodation->city) }}" />
+            </div>
 
-    <div class="form-group">
-      <label for="province">Provincia</label>
-      <input
-        type="text"
-        name="province"
-        id="province"
-        cols="30"
-        rows="10"
-        class="form-control"
-        value="{{ old('province', $accomodation->province) }}"
-      />
-    </div>
+            <div class="form-group col-lg-3 col-md-6">
+                <label for="province">Provincia</label>
+                <input type="text" name="province" id="province" cols="30" rows="10" class="form-control" value="{{ old('province', $accomodation->province) }}" />
+            </div>
 
-    <div class="form-group">
-      <label for="zip">CAP</label>
-      <input
-        type="text"
-        name="zip"
-        id="zip"
-        cols="30"
-        rows="10"
-        class="form-control"
-        value="{{ old('zip', $accomodation->zip) }}"
-      />
-    </div>
+            <div class="form-group col-lg-3 col-md-6">
+                <label for="zip">CAP</label>
+                <input type="text" name="zip" id="zip" cols="30" rows="10" class="form-control" value="{{ old('zip', $accomodation->zip) }}" />
+            </div>
+        </div>
 
-    <div class="form-group">
-      <label for="street_name">Tipo di via</label>
-      <select id="type_street" name="type_street">
-        <option value="via" selected>Via</option>
-        <option value="piazza">Piazza</option>
-        <option value="vicolo">Vicolo</option>
-      </select>
-    </div>
+        <div class="row">
+            <div class="form-group col-lg-3">
+                <label for="street_name">Tipo di via</label>
+                <div class="type">
+                    <select id="type_street" name="type_street" class="form-control">
+                        <option value="via" selected>Via</option>
+                        <option value="piazza">Piazza</option>
+                        <option value="vicolo">Vicolo</option>
+                    </select>
+                </div>
+            </div>
 
-    <div class="form-group">
-      <label for="street_name">Nome della via</label>
-      <input
-        type="text"
-        name="street_name"
-        id="street_name"
-        cols="30"
-        rows="10"
-        class="form-control"
-        value="{{ old('street_name', $accomodation->street_name) }}"
-        
-      />
-    </div>
+            <div class="form-group col-lg-3">
+                <label for="street_name">Nome della via</label>
+                <input type="text" name="street_name" id="street_name" cols="30" rows="10" class="form-control" value="{{ old('street_name', $accomodation->street_name) }}" />
+            </div>
 
-    <div class="form-group">
-      <label for="building_number">Numero Civico</label>
-      <input
-        type="number"
-        name="building_number"
-        id="building_number"
-        cols="30"
-        rows="10"
-        class="form-control"
-        value="{{ old('building_number', $accomodation->building_number) }}"
-      />
-    </div>
+            <div class="form-group col-lg-3">
+                <label for="building_number">Numero Civico</label>
+                <input type="number" name="building_number" id="building_number" cols="30" rows="10" class="form-control" value="{{ old('building_number', $accomodation->building_number) }}" />
+            </div>
+        </div>
 
-    <div class="form-group" >
-      <input
-      id="newAccomodation"
-        type="submit"
-        value="CREA"
-        class="form-control btn btn-success text-capitalize"
-      />
-    </div>
-  </div>
 
-    </form>
+        <div class="form-group form-btn">
+            <input id="newAccomodation" type="submit" value="MODIFICA" class="form-control btn btn-success text-capitalize" />
+        </div>
+</div>
+
+</form>
 </div>
 
 @endsection
