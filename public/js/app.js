70d2efb6de8a0b6d6f7d8df6525199a3020f0735
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-08-02 09:13:14 +0200

    redirect when you add additional images after the first one now goes to logged show

--- public/js/app.js
+++ public/js/app.js
@@ -53404,8 +53404,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Boolean\class-33\exercises\boolbnb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Boolean\class-33\exercises\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\caval\Desktop\Finalproject\boolbnb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\caval\Desktop\Finalproject\boolbnb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
